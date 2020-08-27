d48ef85db986a481187385126691e6af8df29f14
Dev Kev <69854056+DevKevYT@users.noreply.github.com>
2020-08-27 18:05:51 +0200

    Delete ExitCodes.java

--- src/com/devkev/devscript/raw/ExitCodes.java
+++
@@ -1,9 +0,0 @@
-package com.devkev.devscript.raw;
-
-public interface ExitCodes {
-    public static final byte NOTHING = -1;
-    public static final byte ERROR = 0;
-    public static final byte WARNING = 1;
-    public static final byte EMPTY = 2;
-    public static final byte NOT_COMPILED_YET = 3;
-}
