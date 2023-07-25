541e60d986e3a4dbe0baefd6d46395d20ec6762c
Philipp Gersch <devkev@ddbot.top>
2023-07-25 09:07:22 +0200

    Delete com/devkev directory

--- com/devkev/devscript/raw/ApplicationListener.java
+++
@@ -1,6 +0,0 @@
-package com.devkev.devscript.raw;
-
-public interface ApplicationListener {
-	/**Exit codes are: <br>{@link Block#DONE}<br> {@link Block#ERROR}*/
-	public void done(int exitCode);
-}
